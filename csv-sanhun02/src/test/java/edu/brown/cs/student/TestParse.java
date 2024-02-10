836a971f5af9477432f2cc35e485106b66c2a38f
elybrayboy <117125005+elybrayboy@users.noreply.github.com>
2024-02-10 15:05:40 -0500

    test

---
+++ csv-sanhun02/src/test/java/edu/brown/cs/student/TestParse.java
@@ -0,0 +1,49 @@
+package edu.brown.cs.student;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import edu.brown.cs.student.main.Astro;
+import edu.brown.cs.student.main.Builder;
+import edu.brown.cs.student.main.CSVParser;
+import edu.brown.cs.student.main.FactoryFailureException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class TestParse {
+
+  @Test
+  public void testBasic() throws IOException, FactoryFailureException {
+    Reader testReader =
+        new FileReader(
+            "/Users/elybrayboy/Desktop/cs32/sprints/csv-elybrayboy/data/malformed/malformed_signs.csv") {};
+    Builder builder = new Builder();
+
+    CSVParser<Astro> astroCSVParser = new CSVParser<>(testReader, builder, true);
+    List<Astro> astroList = astroCSVParser.Parse();
+    System.out.println(astroList);
+    assertEquals("Annie", astroList.get(0).name);
+    assertEquals("Aries", astroList.get(0).starsign);
+
+    assertEquals("Nicole", astroList.get(4).name);
+    assertEquals("Scorpio", astroList.get(4).starsign);
+
+    assertEquals("Danny", astroList.get(7).name);
+    assertEquals("Pisces", astroList.get(7).starsign);
+  }
+
+  @Test
+  public void testErrorHandling() throws IOException, FactoryFailureException {
+    //    Reader testReader =
+    //        new FileReader(
+    //
+    // "/Users/elybrayboy/Desktop/cs32/sprints/csv-elybrayboy/data/malformed/malforhmed_signs.csv")
+    // {};
+    //    Builder builder = new Builder();
+    //
+    //    CSVParser<Astro> astroCSVParser = new CSVParser<>(testReader, builder, true);
+    //    List<Astro> astroList = astroCSVParser.Parse();
+  }
+}
---
+++ csv-sanhun02/src/test/java/edu/brown/cs/student/TestSearch.java
@@ -0,0 +1,63 @@
+package edu.brown.cs.student;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import edu.brown.cs.student.main.FactoryFailureException;
+import edu.brown.cs.student.main.Util;
+import java.io.IOException;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class TestSearch {
+
+  @Test
+  public void testBasic() throws IOException, FactoryFailureException {
+    Util testBasic =
+        new Util(
+            "/Users/elybrayboy/Desktop/cs32/sprints/csv-elybrayboy/data/census/income_by_race.csv",
+            "White",
+            1,
+            true);
+    List<List<String>> getResults = testBasic.getResults();
+    assertEquals(40, testBasic.getResults().size());
+    for (List<String> ele : getResults) {
+      assertEquals("White", ele.get(1));
+    }
+  }
+
+  @Test
+  public void testEarnings() throws IOException, FactoryFailureException {
+    Util earningsTest =
+        new Util(
+            "/Users/elybrayboy/Desktop/cs32/sprints/csv-elybrayboy/data/census/dol_ri_earnings_disparity.csv",
+            "RI",
+            null,
+            true);
+    List<List<String>> getResults = earningsTest.getResults();
+
+    assertEquals(6, getResults.size());
+  }
+
+  @Test
+  public void testEducation() throws IOException, FactoryFailureException {
+    Util eduTest =
+        new Util(
+            "/Users/elybrayboy/Desktop/cs32/sprints/csv-elybrayboy/data/census/postsecondary_education.csv",
+            "Brown University",
+            null,
+            true);
+    List<List<String>> getResults = eduTest.getResults();
+
+    assertEquals(16, getResults.size());
+
+    Util eduTestEmpty =
+        new Util(
+            "/Users/elybrayboy/Desktop/cs32/sprints/csv-elybrayboy/data/census/postsecondary_education.csv",
+            "hello",
+            null,
+            true);
+    List<List<String>> getResultsEmpty = eduTestEmpty.getResults();
+
+    assertEquals(0, getResultsEmpty.size());
+  }
+}
